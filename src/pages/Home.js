b859082cba813b738ebf20540867fc48f106bc6b
hypermassive <hypermassive1@gmail.com>
2018-07-29 22:01:22 +0100

    installed react-formio, made page layout changes.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 function Home() {
-  return <div>Home Page.</div>;
+  return (
+    <div>
+      <Typography variant="display3">Home</Typography>
+      <Typography variant="headline">
+        Welcome! there&apos;s nowt on this page.
+      </Typography>
+    </div>
+  );
 }
 
 export default Home;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 function Login() {
-  return <div>Login Page.</div>;
+  return (
+    <div>
+      <Typography variant="display3">Login</Typography>
+      <Typography variant="headline">
+        Authenticate with the formio backend.
+      </Typography>
+    </div>
+  );
 }
 
 export default Login;
